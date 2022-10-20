33eb62e0fb87ab9583afa2e135abc6c5004bee00
chuge (mproveme) <ruslankum2@gmail.com>
2022-10-20 21:21:20 +0300

    ex01 started

---
+++ ex01/ScavTrap.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mproveme <mproveme@student.21-school.ru    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/20 15:27:08 by mproveme          #+#    #+#             */
+/*   Updated: 2022/10/20 15:27:58 by mproveme         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCAVTRAP_HPP
+# define SCAVTRAP_HPP
+
+#include <iostream>
+
+class ScavTrap
+{
+private:
+	/* data */
+public:
+	ScavTrap(/* args */);
+	~ScavTrap();
+};
+
+ScavTrap::ScavTrap(/* args */)
+{
+}
+
+ScavTrap::~ScavTrap()
+{
+}
+
+
+#endif
